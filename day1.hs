d440bb37e85e7b64248d087d6f0403aaf7eb783f
zachary munro <zach.munro@smartsheet.com>
2021-12-01 14:22:40 -0500

    cleaned up code a bit

--- day1.hs
+++ day1.hs
@@ -2,16 +2,14 @@ import System.IO
 import Control.Monad
 
 main = do
-        let list = []
-        contents <- readFile "puzzle-input/day1-part1.txt"
-        let singlewords = words contents
-            list = f singlewords
-            sum = foldl1 (\acc x -> acc + x)
-            ans = snd $ foldl (\(prev, inc) x -> (x, (inc+) . fromEnum $ x > prev)) ((head list), 0) list
-            ans2 = snd $ foldl (\(prevs, inc) x -> ((tail prevs) ++ [x], (inc+) . fromEnum $ ((sum(tail prevs) + x) > (sum prevs)))) (take 3 list, 0) (drop 3 list)
-        print ans
-        print ans2
-f :: [String] -> [Int]
-f = map read
+  xs <- map read . lines <$> readFile "puzzle-input/day01.txt"
+  putStrLn $ "part 1: " <> show (part1 xs)
+  putStrLn $ "part 2: " <> show (part2 xs)
 
+sumList :: [Int] -> Int
+sumList [] = 0
+sumlist (x:xs) = x + sumList xs
+
+part1 list = snd $ foldl (\(prev, inc) x -> (x, (inc+) . fromEnum $ x > prev)) ((head list), 0) list
+part2 list = snd $ foldl (\(prevs, inc) x -> ((tail prevs) ++ [x], (inc+) . fromEnum $ ((sum(tail prevs) + x) > (sum prevs)))) (take 3 list, 0) (drop 3 list)
 
