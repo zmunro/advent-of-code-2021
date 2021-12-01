f69db823e341686b6b20fabe067a06f89722bed9
zachary munro <zach.munro@smartsheet.com>
2021-12-01 14:39:53 -0500

    added code golfed version

--- day1.hs
+++ day1.hs
@@ -9,3 +9,6 @@ main = do
 part1 xs = snd $ foldl (\(prev, inc) x -> (x, (inc+) . fromEnum $ x > prev)) ((head xs), 0) xs
 part2 xs = snd $ foldl (\(prevs, inc) x -> ((tail prevs) ++ [x], (inc+) . fromEnum $ ((sum(tail prevs) + x) > (sum prevs)))) (take 3 xs, 0) (drop 3 xs)
 
+
+-- code golfed part 2
+P xs = snd $ foldl (\(p, i) x -> ((tail p) ++ [x], (i+) . fromEnum $ ((sum(tail p) + x) > (sum p)))) (take 3 xs, 0) (drop 3 xs)
