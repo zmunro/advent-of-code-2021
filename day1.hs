1ee335e3b21c4cd871890ec8645894628cd0b322
zachary munro <zach.munro@smartsheet.com>
2021-12-01 14:31:28 -0500

    removed sumlist helper func

--- day1.hs
+++ day1.hs
@@ -6,10 +6,6 @@ main = do
   putStrLn $ "part 1: " <> show (part1 xs)
   putStrLn $ "part 2: " <> show (part2 xs)
 
-sumList :: [Int] -> Int
-sumList [] = 0
-sumlist (x:xs) = x + sumList xs
-
 part1 xs = snd $ foldl (\(prev, inc) x -> (x, (inc+) . fromEnum $ x > prev)) ((head xs), 0) xs
 part2 xs = snd $ foldl (\(prevs, inc) x -> ((tail prevs) ++ [x], (inc+) . fromEnum $ ((sum(tail prevs) + x) > (sum prevs)))) (take 3 xs, 0) (drop 3 xs)
 
