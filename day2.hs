2952d88d7c7b7b873a7670d8fe317f3babe64a5d
zachary munro <zach.munro@smartsheet.com>
2021-12-02 15:52:13 -0500

    finished day 2

--- day2.hs
+++ day2.hs
@@ -1,9 +1,14 @@
 main :: IO ()
 main = do
   xs <- map (sndToInt . words) . lines <$> readFile "puzzle-input/day02.txt"
-  let ans = foldl navigate (0, 0) xs
+  let ans = foldl part1 (0, 0) xs
+  let ans2 = foldl part2 (0,0,0) xs
   print ans
   print (uncurry (*) ans)
+  print ans2
+  print ((\(a,b,_) -> a * b) ans2)
+
+
 
 
 sndToInt :: [String] -> (String, Int)
@@ -11,8 +16,14 @@ sndToInt [str, n] = (str, read n :: Int)
 sndToInt _ = ("", 0)
 
 
-navigate :: Num a => (a, a) -> ([Char], a) -> (a, a)
-navigate (depth, lat) ("forward", distance) = (depth, lat + distance)
-navigate (depth, lat) ("down", distance) = (depth + distance, lat)
-navigate (depth, lat) ("up", distance) = (depth - distance, lat)
-navigate (depth, lat) (_, distance) = (0, 0)
+part1 :: Num a => (a, a) -> ([Char], a) -> (a, a)
+part1 (depth, lat) ("forward", distance) = (depth, lat + distance)
+part1 (depth, lat) ("down", distance) = (depth + distance, lat)
+part1 (depth, lat) ("up", distance) = (depth - distance, lat)
+part1 (depth, lat) (_, distance) = (0, 0)
+
+part2 :: Num a => (a, a, a) -> ([Char], a) -> (a, a, a)
+part2 (depth, lat, aim) ("forward", distance) = (depth + (aim * distance), lat + distance, aim)
+part2 (depth, lat, aim) ("down", distance) = (depth, lat, aim + distance)
+part2 (depth, lat, aim) ("up", distance) = (depth, lat, aim - distance)
+part2 (depth, lat, aim) (_, distance) = (0, 0, 0)
