f3c7f0115394675b29fb5c51b0a093db6d680f6e
zachary munro <zach.munro@smartsheet.com>
2021-12-02 13:24:47 -0500

    cleaned up day 1 code

--- day1.hs
+++ day1.hs
@@ -6,9 +6,9 @@ main = do
   putStrLn $ "part 1: " <> show (part1 xs)
   putStrLn $ "part 2: " <> show (part2 xs)
 
-part1 xs = snd $ foldl (\(prev, inc) x -> (x, (inc+) . fromEnum $ x > prev)) ((head xs), 0) xs
-part2 xs = snd $ foldl (\(prevs, inc) x -> ((tail prevs) ++ [x], (inc+) . fromEnum $ ((sum(tail prevs) + x) > (sum prevs)))) (take 3 xs, 0) (drop 3 xs)
+part1 xs = snd $ foldl (\(prev, inc) x -> (x, (inc+) . fromEnum $ x > prev)) (head xs, 0) xs
+part2 xs = snd $ foldl (\(prevs, inc) x -> (tail prevs ++ [x], (inc+) . fromEnum $ ((sum(tail prevs) + x) > sum prevs))) (take 3 xs, 0) (drop 3 xs)
 
 
 -- code golfed part 2
-f xs = snd $ foldl (\(p, i) x -> ((tail p) ++ [x], (i+) . fromEnum $ ((sum(tail p) + x) > (sum p)))) (take 3 xs, 0) (drop 3 xs)
+f xs = snd $ foldl (\(p, i) x -> (tail p ++ [x], (i+) . fromEnum $ ((sum(tail p) + x) > sum p))) (take 3 xs, 0) (drop 3 xs)
